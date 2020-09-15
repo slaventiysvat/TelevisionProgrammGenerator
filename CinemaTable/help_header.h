fcf8d608b3a7ddcedde367713ad6d76e8f296cd2
Sviatoslav Starokozhev <sviatoslav.starokozhev@p-product.com>
2020-09-15 22:48:41 +0300

    added getters and setters for class page creator

--- CinemaTable/help_header.h
+++ CinemaTable/help_header.h
@@ -1,10 +1,8 @@
 #ifndef HELP_HEADER
 #define HELP_HEADER
-
-
-
-
-
-
-
+#include "programm.h"
+#include "programm_list.h"
+#include "spdlog/spdlog.h"
+#include "spdlog/cfg/env.h" // for loading levels from the environment variable
+#include <string>
 #endif/*HELP_HEADER*/
