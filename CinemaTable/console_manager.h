db31cade613f6ca2150df2409c9001e5d92a57f3
Sviatoslav Starokozhev <sviatoslav.starokozhev@p-product.com>
2020-09-16 18:47:53 +0300

    class page_bulder was modified, added color output, test main

---
+++ CinemaTable/console_manager.h
@@ -0,0 +1,12 @@
+#ifndef CONSOLE_MANAGER
+#define CONSOLE_MANAGER
+#include <string>
+
+void string_initializer(std::string name_of_programm,
+                        std::string channel,
+                        std::string day_of_programm,
+                        std::string time_of_programm,
+                        std::string name_of_cahnnel);
+
+
+#endif/*CONSOLE_MANAGER*/
--- CinemaTable/page_builder.cpp
+++ CinemaTable/page_builder.cpp
@@ -1,9 +1,7 @@
 #include "page_builder.h"
 #include "help_header.h"
 #include <fstream>
-void page_creator(page_builder::TV_program_init page_init,std::string name_of_html_page) {
-
-
+void page_creator(std::string name_of_html_page) {
 
 
 
@@ -11,55 +9,50 @@ void page_creator(page_builder::TV_program_init page_init,std::string name_of_ht
 
 }
 
-std::string get_name_of_programm(page_builder::TV_program_init page_init) {
-
+std::string page_builder::get_name_of_programm() {
+    
     return page_init.name_of_programm;
 }
-std::string get_channel(page_builder::TV_program_init page_init) {
+std::string page_builder::get_channel() {
 
     return page_init.channel;
 }
-std::string get_day_of_programm(page_builder::TV_program_init page_init) {
+std::string page_builder::get_day_of_programm() {
 
     return page_init.day_of_programm;
 }
-std::string get_time_of_programm(page_builder::TV_program_init page_init) {
+std::string page_builder::get_time_of_programm() {
 
     return page_init.time_of_programm;
 }
-std::string get_name_of_cahnnel(page_builder::TV_program_init page_init) {
+std::string page_builder::get_name_of_cahnnel() {
 
     return page_init.name_of_cahnnel;
 }
 
-page_builder::TV_program_init set_name_of_programm(page_builder::TV_program_init page_init, 
-    std::string set_name_of_programm) {
+void page_builder::set_name_of_programm(std::string set_name_of_programm) {
 
     page_init.name_of_programm = set_name_of_programm;
-    return page_init;
+
 }
-page_builder::TV_program_init set_channel(page_builder::TV_program_init page_init, 
-    std::string set_channel) {
+void  page_builder::set_channel(std::string set_channel) {
 
     page_init.channel = set_channel;
-    return page_init;
+ 
 }
-page_builder::TV_program_init set_day_of_programm(page_builder::TV_program_init page_init,
-    std::string set_day_of_programm) {
+void page_builder::set_day_of_programm(std::string set_day_of_programm) {
 
 
     page_init.day_of_programm = set_day_of_programm;
-    return page_init;
+ 
 }
-page_builder::TV_program_init set_time_of_programm(page_builder::TV_program_init page_init,
-    std::string set_time_of_programm) {
+void page_builder::set_time_of_programm(std::string set_time_of_programm) {
 
     page_init.time_of_programm = set_time_of_programm;
-    return page_init;
+
 }
-page_builder::TV_program_init set_name_of_cahnnel(page_builder::TV_program_init page_init,
-    std::string set_name_of_cahnnel) {
+void page_builder::set_name_of_cahnnel(std::string set_name_of_cahnnel) {
 
     page_init.name_of_cahnnel = set_name_of_cahnnel;
-    return page_init;
+   
 }
